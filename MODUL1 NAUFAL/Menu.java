01768e8d8d9a612e80f49e88dfec886bf068f1ed
falkrii <116436753+falkrii@users.noreply.github.com>
2022-11-04 08:13:38 +0700

    Add files via upload

---
+++ MODUL1 NAUFAL/Menu.java
@@ -0,0 +1,23 @@
+public class Menu {
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+    public String Name;
+
+    public String Category;
+
+    public int Price;
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public void setCategory(String category) {
+        this.Category = category;
+    }
+
+    public void setPrice(int price) {
+        this.Price = price;
+    }
+
+}
---
+++ MODUL1 NAUFAL/Server.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO Create Database
+
+
+        // TODO Create Menu
+        Menu Menu1 = new Menu();
+        Menu1.setName("bakso");
+        Menu1.setCategory("(makanan)");
+        Menu1.setPrice(10000);
+
+        Menu Menu2 = new Menu()
+        Menu2.setName("es teh");
+        Menu2.setCategory("(minuman)");
+        Menu2.setPrice(5000);
+
+        Menu Menu3 = new Menu()
+        Menu3.setName("brownies");
+        Menu3.setCategory("(dessert)");
+        Menu3.setPrice(15000);
+
+        
+
+        // TODO Insert Menu to Database
+
+        
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
+    }
+}
